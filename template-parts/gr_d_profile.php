d86454c1bc5b8e1ad671ebbe19652413d46174fa
b6016323 <b6016323@my.shu.ac.uk>
2018-02-16 15:15:06 +0000

    building profile page
    does not have styling or map yet

--- template-parts/gr_d_profile.php
+++ template-parts/gr_d_profile.php
@@ -5,10 +5,25 @@
 get_header();
 
 $businessid = get_query_var('business');
+$business = get_user_by('id',$businessid);
+$businessmeta = get_user_meta($businessid);
 
+/*
+echo "<pre>";
+print_r($business);
+echo "</pre>";
+
+echo "<pre>";
+print_r($businessmeta);
+echo "</pre>";
+*/
 ?>
 <div class="groak_business_profile">
-    <h2><?php echo $businessid; ?></h2>
+    <h2><?php echo $businessmeta['business_name'][0]; ?></h2>
+    <div class="groak_business_profile_nav">
+    <ul><li><a href="menu">Menu</a></li><li><a href="contact">Contact</a></li></ul>
+        <div class="groak_business_profile_about"><?php echo $businessmeta['description'][0];?></div>
+    </div>
 </div>
 <?php
 get_footer();
