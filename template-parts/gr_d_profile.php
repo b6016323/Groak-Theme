3f4ac4643a7aebbad347a13b6a027987eeee575a
b6016323 <b6016323@my.shu.ac.uk>
2018-02-16 16:14:05 +0000

    just messing around further.
    should be cleaned up

--- template-parts/gr_d_profile.php
+++ template-parts/gr_d_profile.php
@@ -9,26 +9,18 @@ $business = get_user_by('id',$businessid);
 $businessmeta = get_user_meta($businessid);
 
 $business_address = $businessmeta['business_name'][0].",".$businessmeta['business_address'][0].",".$businessmeta['business_postcode'][0].",".$businessmeta['business_country'][0];
-echo $business_address;
 $business_address = str_replace(' ','%20',$business_address);
-echo $business_address;
 $map_url = "https://www.google.com/maps/embed/v1/search?key=AIzaSyCNDrILVNLzWGASvtl8TzfVs-YW0p94Zn4&q=".$business_address;
-/*
-echo "<pre>";
-print_r($business);
-echo "</pre>";
 
-echo "<pre>";
-print_r($businessmeta);
-echo "</pre>";
-*/
+$curr_url = home_url($wp->request);
+
 ?>
 <div class="groak_business_profile">
     <h2><?php echo $businessmeta['business_name'][0]; ?></h2>
     <div class="groak_business_profile_nav">
         <ul>
-            <li><a href="menu">Menu</a></li>
-            <li><a href="contact">Contact</a></li>
+            <li><a href="<?php echo $curr_url?>/menu">Menu</a></li>
+            <li><a href="<?php echo $curr_url?>/contact">Contact</a></li>
         </ul>
     </div>
     <div class="groak_business_profile_about"><?php echo $businessmeta['description'][0];?></div>
