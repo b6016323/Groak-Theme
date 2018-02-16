acc31b3b1cf41f1f5e25ef5d0bc4220a2681cb9f
b6016323 <b6016323@my.shu.ac.uk>
2018-02-16 16:05:36 +0000

    google maps attempt
    There's some problems with the maps not showing the correct result.  An
    additional map shows the previous result.

--- template-parts/gr_d_profile.php
+++ template-parts/gr_d_profile.php
@@ -8,6 +8,11 @@ $businessid = get_query_var('business');
 $business = get_user_by('id',$businessid);
 $businessmeta = get_user_meta($businessid);
 
+$business_address = $businessmeta['business_name'][0].",".$businessmeta['business_address'][0].",".$businessmeta['business_postcode'][0].",".$businessmeta['business_country'][0];
+echo $business_address;
+$business_address = str_replace(' ','%20',$business_address);
+echo $business_address;
+$map_url = "https://www.google.com/maps/embed/v1/search?key=AIzaSyCNDrILVNLzWGASvtl8TzfVs-YW0p94Zn4&q=".$business_address;
 /*
 echo "<pre>";
 print_r($business);
@@ -21,8 +26,14 @@ echo "</pre>";
 <div class="groak_business_profile">
     <h2><?php echo $businessmeta['business_name'][0]; ?></h2>
     <div class="groak_business_profile_nav">
-    <ul><li><a href="menu">Menu</a></li><li><a href="contact">Contact</a></li></ul>
-        <div class="groak_business_profile_about"><?php echo $businessmeta['description'][0];?></div>
+        <ul>
+            <li><a href="menu">Menu</a></li>
+            <li><a href="contact">Contact</a></li>
+        </ul>
+    </div>
+    <div class="groak_business_profile_about"><?php echo $businessmeta['description'][0];?></div>
+    <div class="groak_business_profile_map">
+        <iframe width="600" height="450" frameborder="0" style="border:0" src="<?php echo $map_url;?>" allowfullscreen></iframe>
     </div>
 </div>
 <?php
