eb03d51ac037c8569086b3cbb35a5a06009396ac
b6016323 <b6016323@my.shu.ac.uk>
2018-03-07 09:55:00 +0000

    custom logo changes

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -39,17 +39,26 @@ function groak_theme_logo()
     //first check if there is a custom logo set, we dont want to override the custom logo, it may be done on purpose
     if(!has_custom_logo())
     {
+		
         //Custom logo not set, we get the location, country and then assign it to an image.  EG: gr_France.png.  All images will need to be done for that format.  There may be a better way of doing this
         //We do need to check if the user is looking at a different location and change the logo based on that.
         //These if/else statements just return a home link with an image inside.
 	$user_location = geoip_detect2_get_info_from_current_ip();
 	$country = $user_location->country->name;
 	$country_for_image = str_replace(" ","",$country);
-    if($country_for_image = "" | empty($country_for_image))
+    if($country_for_image == "" | empty($country_for_image))
     {
         $country_for_image = "Default";
     }
-	$logo_src = get_template_directory_uri()."/inc/images/gr_".$country_for_image.".png";
+	//NEEDS TIDYING UP but it checks if the file exists,if not just show the default
+		if(!file_exists($logo_src))
+		{
+			$logo_src = get_template_directory_uri()."/inc/images/gr_Default.png";
+		}
+		else
+		{
+			$logo_src = get_template_directory_uri()."/inc/images/gr_".$country_for_image.".png";
+		}
     }
     else
     {
